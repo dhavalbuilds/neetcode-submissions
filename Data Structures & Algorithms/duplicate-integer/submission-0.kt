b73a4fbdfc708264b47b9c410fac1ac31d1b944b
Dhaval Patel <dhavalpatel349@gmail.com>
2026-05-01 21:34:36 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun hasDuplicate(nums: IntArray): Boolean {
+        val map = mutableMapOf<Int, Int>()
+        nums.forEach{ i->
+        if(map.containsKey(i))
+            return true
+        else
+            map.put(i,1)
+        }
+        return false
+    }
+}
